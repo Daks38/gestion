0af5e52a352680c1c4911e56b3f2bd8acd8d97ce
DAKS54 <sylvain.diallo@epitech.eu>
2026-01-18 19:26:36 +0000

    [Add] Project gestion

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use("/tasks",router)
 app.get("/", (req, res)=>{
     res.send("Tout fonctionne bien ici")
 })
-module.exports = app
-// app.listen(process.env.PORT, ()=>console.log(`Vous êtes connecté sur http://localhost:${process.env.PORT}`))
+// module.exports = app
+app.listen(process.env.PORT, ()=>console.log(`Vous êtes connecté sur http://localhost:${process.env.PORT}`))
